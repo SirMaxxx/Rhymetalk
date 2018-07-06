1567c555ce18e54d31a94cfac250036de6c47452
SirMaxxx <mailmaxxx@gmail.com>
2018-07-06 21:21:09 +1000

    Installed Angular Material Coded Text-Input Created Base-Component

--- projects/components/src/lib/text-input/text-input.component.ts
+++ projects/components/src/lib/text-input/text-input.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { BaseComponent } from '../base.component';
 
 @Component({
-  selector: 'max-text-input',
+  selector: 'max-textinput',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css']
+  styleUrls: ['./text-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextInputComponent),
+      multi: true
+    }
+  ]
 })
-export class TextInputComponent implements OnInit {
+export class TextInputComponent extends BaseComponent implements OnInit {
+  /** The label shown in front of the text box */
+  @Input() placeholder = '';
+  @Input() talk = false;
 
-  constructor() { }
+  synUtterance = new SpeechSynthesisUtterance();
 
-  ngOnInit() {
+  constructor() {
+    super();
   }
 
+  //#region Angular implementations
+  ngOnInit() {
+    this.synUtterance.voice = speechSynthesis.getVoices().filter(voice => {
+      return voice.name === 'Karen';
+    })[0];
+
+    this.synUtterance.lang = 'en-us';
+    this.synUtterance.volume = 1;
+    this.synUtterance.rate = 1;
+    console.log('Can ii talk? ', this.talk);
+  }
+  //#endregion
+
+  //#region DOM event handlers
+  /** Called from DOM when value is changed */
+  valueChanged(event: any) {
+    this.value = event.target.value;
+    this.onChange(event.target.value);
+  }
+
+  /** Called from DOM when control is touched */
+  blurred() {
+    console.log('focussed!');
+    if (this.value && this.value !== '') {
+      this.speak('You entered ' + this.value);
+    }
+  }
+  focussed() {
+    console.log('blurred!');
+
+    this.onTouched();
+
+    this.speak('Please enter ' + this.placeholder);
+  }
+  //#endregion
+
+  speak(textToSpeech: string) {
+    console.log('Saying ', textToSpeech);
+
+    this.synUtterance.text = textToSpeech;
+
+    // synUtterance.pitch = parseFloat(pitchRange.value);
+
+    if (this.talk) {
+      window.speechSynthesis.speak(this.synUtterance);
+    }
+  }
 }
