98d6cec6b7e6c2fdcee8c34dd5113522909484d2
SirMaxxx <mailmaxxx@gmail.com>
2018-07-06 21:17:46 +1000

    Added Text Input component

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ComponentsComponent } from './components.component';
+import { TextInputComponent } from './text-input/text-input.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [ComponentsComponent],
+  declarations: [ComponentsComponent, TextInputComponent],
   exports: [ComponentsComponent]
 })
 export class ComponentsModule { }
