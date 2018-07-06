b86a584d84660768c935dac057ba5aa2e7015487
SirMaxxx <mailmaxxx@gmail.com>
2018-07-06 21:25:43 +1000

    Updated module to import/export text input component

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentsComponent } from './components.component';
+import { MaxMaterialModele } from './material.module';
 import { TextInputComponent } from './text-input/text-input.component';
 
 @NgModule({
-  imports: [
-  ],
+  imports: [, BrowserAnimationsModule, MaxMaterialModele],
   declarations: [ComponentsComponent, TextInputComponent],
-  exports: [ComponentsComponent]
+  exports: [ComponentsComponent, TextInputComponent]
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
